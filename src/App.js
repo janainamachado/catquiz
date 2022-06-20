16960452b89ff614c3fbea65a2299ea246ff609d
Janaina Machado <pessanha.janaina@gmail.com>
2022-06-20 16:51:47 +0100

    initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Row, Text } from '@nextui-org/react';
+import Question from './components/Question';
+
+const catApiKey = process.env.REACT_APP_CAT_API_KEY
 
 function App() {
+
+  const emptyRandomCat = { url: 'empty' }
+  const [randomCatFirst, setRandomCatFirst] = useState(emptyRandomCat)
+  const [randomCatSecond, setRandomCatSecond] = useState(emptyRandomCat)
+  const [voteCounter, setVoteCounter] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState('')
+  const [currentResult, setCurrentResult] = useState('')
+  const requiredAnswersNumber = 3;
+
+  useEffect(() => {
+    if(voteCounter >= requiredAnswersNumber) {
+      const randomResultIndex = Math.floor(Math.random() * results.length); 
+      setCurrentResult(results[randomResultIndex])
+    }
+  }, [voteCounter])
+  
+  const getRandomCats = async () => {
+    const randomCat = await fetch('https://api.thecatapi.com/v1/images/search', {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': catApiKey 
+      }
+    })
+    .then((response) => response.json())
+    
+    return randomCat[0]
+  }
+
+  const renderRandomQuestion = async () => {
+    setRandomCatFirst(await getRandomCats())
+    setRandomCatSecond(await getRandomCats())
+    
+    const randomQuestionIndex = Math.floor(Math.random() * questions.length); 
+    setCurrentQuestion(questions[randomQuestionIndex])
+  }
+
+  const vote = () => {
+    setVoteCounter(voteCounter+1)
+    renderRandomQuestion()
+  }
+  
+  const questions = [
+    "Which cat is you animal spirit?",
+    "Which cat defines your humour right now?",
+    "Which cat is your favorite?"
+  ]
+
+  const results = [
+    "Congrats, you're a lazy cat!",
+    "You enjoy life just like Garfield!", 
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Grid.Container gap={2} justify="center">
+        <Row justify="center">
+          <Grid>
+            <Button color="gradient" onClick={renderRandomQuestion}>What type of cat am I?</Button>
+          </Grid>
+        </Row>
+      </Grid.Container>
+      
+      {voteCounter < requiredAnswersNumber &&
+        <Question 
+          randomCatFirst={randomCatFirst}
+          randomCatSecond={randomCatSecond}
+          currentQuestion={currentQuestion}
+          vote={vote}
+        />
+      }
+      {voteCounter >= requiredAnswersNumber &&
+        <Text
+          h2 
+          css={{
+            textAlign: "center",
+            textGradient: "45deg, $purple600 -20%, $pink600 100%",
+          }}
+          weight="bold"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {currentResult}
+        </Text>
+      }
+
+
+    </>
   );
 }
 
---
+++ src/components/Question.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Grid, Image, Link, Row, Text } from '@nextui-org/react';
+
+function Question(props) {
+
+  return (
+    <Grid.Container justify="center">
+      <Row justify="center">
+        <Grid>
+          <Text color="#ff4ecd">
+          {props.currentQuestion}
+          </Text>
+        </Grid>
+      </Row>
+      <Grid>
+        <Link onClick={props.vote}>
+          <Image
+          width={500}
+          height={500}  
+          src={props.randomCatFirst.url}/>
+        </Link>
+
+        <Link onClick={props.vote}>
+          <Image 
+          width={500}
+          height={500}  
+          src={props.randomCatSecond.url}/>
+        </Link>
+      </Grid>
+
+    </Grid.Container>
+  )
+}
+
+export default Question;
